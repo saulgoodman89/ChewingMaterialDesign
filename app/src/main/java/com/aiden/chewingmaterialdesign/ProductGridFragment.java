1a59c066c73b72c61667b11ea379c85c5af5ca94
eunggyo <eungkyo89@gamil.com>
2021-10-14 19:56:27 +0900

    add Top Bar

--- app/src/main/java/com/aiden/chewingmaterialdesign/ProductGridFragment.java
+++ app/src/main/java/com/aiden/chewingmaterialdesign/ProductGridFragment.java
@@ -2,9 +2,14 @@ package com.aiden.chewingmaterialdesign;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,13 +21,32 @@ public class ProductGridFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_grid, container, false);
+        View view = inflater.inflate(R.layout.fragment_product_grid, container, false);
+
+        // Set up the toolbar
+        setUpToolbar(view);
+        return view;
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.shr_toolbar_menu,menu);
+    }
+
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
     }
 }
